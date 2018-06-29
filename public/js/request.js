67ea2b789cc670582de22c9ae7954f4650fbadf5
Azeez <azeez@scholarx.co>
2018-06-29 16:46:02 +0100

    updates

---
+++ public/js/request.js
@@ -0,0 +1,7 @@
+fetch('https://free.currencyconverterapi.com/api/v5/convert?q=USD_PHP&compact=y')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
