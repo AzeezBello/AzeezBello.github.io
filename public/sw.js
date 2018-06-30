1d33eb65c5dc89b6517c56a091ba6ad775d08b7b
Azeez <azeez@scholarx.co>
2018-06-30 20:47:41 +0100

    back on track

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,19 @@
-var CACHE_NAME = 'currency-converter-v1';
-var urlsToCache =[
-        '/',
-        'css/landing-page.min.css',
-        'img/',
-        'js/currency-converter.js',
-        'favicon.png',
-        'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
-        'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
-        'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js'
-    
-      ];
+importScripts('/cache-polyfill.js');
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/css/css/landing-page.min.css',
+       '/js/currency-converter.js',
+       '/img/',
+       '/sounds/airhorn.mp3'
+     ]);
+   })
+ );
 });
